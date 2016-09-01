0969aa0648be5468f7838a3f8f68505e1b3b678a
Trần Xuân Nam <nam3vj@gmail.com>
2016-09-01 15:21:41 +0700

    update

---
+++ Algorithm/src/day_2/Add.java
@@ -0,0 +1,12 @@
+package day_2;
+
+import java.util.Scanner;
+
+public class Add {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int i = scanner.nextInt();
+		System.out.println(i + 1);
+		scanner.close();
+	}
+}
