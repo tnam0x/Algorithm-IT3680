ab9b569d65f3b4440023e73b5f6f481c9bb0d9ae
Nam Tran <nam3vj@gmail.com>
2016-09-12 23:49:30 +0700

    update

--- Algorithm/src/sorts/MergeSort.java
+++ Algorithm/src/sorts/MergeSort.java
@@ -1,12 +1,13 @@
 package sorts;
+
 /**
  * Độ phức tạp thuật toán: trung bình O(nlogn)
- * */
+ */
 public class MergeSort {
 
 	public static void main(String[] args) {
-		int[] a = { 4, 2, 5, 7, 1, 15, 74, 13, 21 };
-		sort(a, 0, a.length - 1);
+		int[] a = { 4, 2, 5, 10, 7, 1, 15, 74, 13, 21 };
+		sort(a, 0, a.length-1);
 		for (int i = 0; i < a.length; i++) {
 			System.out.print(a[i] + " ");
 		}
@@ -33,36 +34,23 @@ public class MergeSort {
 
 		while ((first1 <= last1) && (first2 <= last2)) {
 			if (array[first1] < array[first2]) {
-				temp[index] = array[first1];
-				index++;
-				first1++;
+				temp[index++] = array[first1++];
 			} else {
-				temp[index] = array[first2];
-				index++;
-				first2++;
+				temp[index++] = array[first2++];
 			}
 		}
 
-		if (first2 > last2) {
-			while (first1 <= last1) {
-				temp[index] = array[first1];
-				index++;
-				first1++;
-			}
+		while (first1 <= last1) {
+			temp[index++] = array[first1++];
 		}
 
-		if (first1 > last1) {
-			while (first2 <= last2) {
-				temp[index] = array[first2];
-				index++;
-				first2++;
-			}
+		while (first2 <= last2) {
+			temp[index++] = array[first2++];
 		}
 
 		for (index = first; index <= last; index++) {
 			array[index] = temp[index];
 		}
-
 	}
 
 }
