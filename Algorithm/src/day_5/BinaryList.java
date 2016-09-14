accfd78836b72866281fca28d4ef2234e8e0241f
Nam Tran <nam3vj@gmail.com>
2016-09-14 09:56:55 +0700

    update

--- Algorithm/src/day_5/BinaryList.java
+++ Algorithm/src/day_5/BinaryList.java
@@ -1,19 +1,31 @@
 package day_5;
 
+import java.util.Scanner;
+
 public class BinaryList {
 	private static int[] a;
-	private static int n;
+	private static String[] items;
+	private static String token;
+	private static int n, k, i, index = 0;
 
-	private static void TRY(int i) {
+	private static void TRY(int j) {
 		for (int v = 0; v <= 1; v++) {
-			a[i] = v;
-			if (i == n - 1) {
-				for (int j = 0; j < n; j++) {
-					System.out.print(a[j]);
+			a[j] = v;
+			if (j == n - 1) {
+				int numberOfZero = 1;
+				token = String.valueOf(a[0]);
+				for (int l = 1; l < n; l++) {
+					if (a[l - 1] == 0 && a[l] == 0) {
+						numberOfZero++;
+						token += a[l];
+					}
 				}
-				System.out.println();
+				if (numberOfZero < i) {
+					items[index++] = token;
+				}
+				System.out.println(token);
 			} else {
-				TRY(i + 1);
+				TRY(j + 1);
 			}
 		}
 	}
@@ -21,11 +33,22 @@ public class BinaryList {
 	private static void list(int n) {
 		BinaryList.n = n;
 		a = new int[n];
+		items = new String[(int) Math.pow(2, n)];
 		TRY(0);
 	}
 
 	public static void main(String[] args) {
-		list(3);
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		k = scanner.nextInt();
+		i = scanner.nextInt();
+		list(n);
+		if (items.length < k) {
+			System.out.println(-1);
+		} else {
+			System.out.println(items[k]);
+		}
+		scanner.close();
 	}
 
 }
