e38b63d0b16d6bfb2f42c47ed0046e2c88e9e3b4
Nam Tran <nam3vj@gmail.com>
2016-09-14 11:38:13 +0700

    update

--- Algorithm/src/day_4/Test.java
+++ Algorithm/src/day_4/Test.java
@@ -1,16 +1,11 @@
 package day_4;
 
-import java.io.FileNotFoundException;
-import java.util.Locale;
-import java.util.Scanner;
-
 public class Test {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		Scanner scanner = new Scanner(System.in);
-		float f = scanner.nextFloat();
-		scanner.useLocale(Locale.CANADA);
-		System.err.println(f);
-		scanner.close();
+	public static void main(String[] args) {
+		String s = "0";
+		if (s.equals("0")) {
+			System.out.println("true");
+		}
 	}
 }
--- Algorithm/src/day_5/BinaryList.java
+++ Algorithm/src/day_5/BinaryList.java
@@ -3,50 +3,68 @@ package day_5;
 import java.util.Scanner;
 
 public class BinaryList {
-	private static int[] a;
-	private static String[] items;
+	private static int[] binary;
 	private static String token;
 	private static int n, k, i, index = 0;
+	private static boolean isSuccess = false;
 
 	private static void TRY(int j) {
 		for (int v = 0; v <= 1; v++) {
-			a[j] = v;
+			binary[j] = v;
 			if (j == n - 1) {
-				int numberOfZero = 1;
-				token = String.valueOf(a[0]);
-				for (int l = 1; l < n; l++) {
-					if (a[l - 1] == 0 && a[l] == 0) {
+				boolean isAdd = true;
+				boolean isReset = false;
+				int numberOfZero = 0;
+				token = "";
+				for (int l = 0; l < n; l++) {
+					token += String.valueOf(binary[l] + " ");
+					if (binary[l] == 0) {
+						if (isReset) {
+							isReset = false;
+							numberOfZero = 0;
+						}
 						numberOfZero++;
-						token += a[l];
+					} else {
+						isReset = true;
+					}
+					if (numberOfZero == i) {
+						isAdd = false;
+						break;
 					}
 				}
-				if (numberOfZero < i) {
-					items[index++] = token;
+
+				if (isAdd) {
+					index++;
+					if (k == index) {
+						System.out.println(token);
+						isSuccess = true;
+						return;
+					}
 				}
-				System.out.println(token);
 			} else {
+				if (isSuccess) {
+					return;
+				}
 				TRY(j + 1);
 			}
 		}
 	}
 
-	private static void list(int n) {
-		BinaryList.n = n;
-		a = new int[n];
-		items = new String[(int) Math.pow(2, n)];
+	private static void list() {
+		binary = new int[n];
 		TRY(0);
 	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
+		n = scanner.nextInt();
 		k = scanner.nextInt();
 		i = scanner.nextInt();
-		list(n);
-		if (items.length < k) {
-			System.out.println(-1);
-		} else {
-			System.out.println(items[k]);
+		if (n <= 10000 && k <= 10000 && i <= 10000) {
+			list();
+			if (!isSuccess) {
+				System.out.println(-1);
+			}
 		}
 		scanner.close();
 	}
