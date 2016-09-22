81ad0d765a057f06477fbefc2932e95f9dd0184c
Nam Tran <nam3vj@gmail.com>
2016-09-23 00:27:53 +0700

    update

--- Other/src/spoj/ARRANGE.java
+++ Other/src/spoj/ARRANGE.java
@@ -8,7 +8,6 @@ import java.io.InputStreamReader;
  *
  */
 public class ARRANGE {
-
 	static int[] a;
 
 	public static void main(String[] args) throws Exception {
@@ -21,31 +20,59 @@ public class ARRANGE {
 			for (int j = 0; j < amplifiers; j++) {
 				a[j] = Integer.parseInt(line[j]);
 			}
-			sort(a);
+			sort(a, 0, amplifiers - 1);
 			printf();
 		}
 	}
 
-	static void sort(int[] a) {
-		for (int i = 0; i < a.length; i++) {
-			for (int j = a.length - 1; j > 0; j--) {
-				if (a[j] > a[j - 1]) {
-					int temp = a[j];
-					a[j] = a[j - 1];
-					a[j - 1] = temp;
-				}
+	public static void sort(int array[], int first, int last) {
+		if (first < last) {
+			int middle = (first + last) / 2;
+			sort(array, first, middle);
+			sort(array, middle + 1, last);
+			merge(array, first, middle, last);
+		}
+	}
+
+	static void merge(int array[], int first, int middle, int last) {
+		int[] temp = new int[last + 1];
+		int first1, last1, first2, last2;
+		int index = first;
+
+		first1 = first;
+		last1 = middle;
+		first2 = middle + 1;
+		last2 = last;
+
+		while ((first1 <= last1) && (first2 <= last2)) {
+			if (array[first1] < array[first2]) {
+				temp[index++] = array[first1++];
+			} else {
+				temp[index++] = array[first2++];
 			}
+		}
+
+		while (first1 <= last1) {
+			temp[index++] = array[first1++];
+		}
+
+		while (first2 <= last2) {
+			temp[index++] = array[first2++];
+		}
+
+		for (index = first; index <= last; index++) {
+			array[index] = temp[index];
 		}
 	}
 
 	static void printf() {
-		for (int i = a.length - 1; i >= 0; i--) {
+		for (int i = 0; i < a.length; i++) {
 			if (a[i] != 1 && a[i] != 2) {
 				break;
 			}
 			System.out.print(a[i] + " ");
 		}
-		for (int i = 0; i < a.length; i++) {
+		for (int i = a.length - 1; i >= 0; i--) {
 			if (a[i] == 1 || a[i] == 2) {
 				break;
 			}
---
+++ Other/src/spoj/Test.java
@@ -0,0 +1,11 @@
+package spoj;
+
+/**
+ * @author namtran
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+	}
+
+}
