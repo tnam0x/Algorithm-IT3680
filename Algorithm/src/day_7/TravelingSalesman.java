f83861adc4a8d3ebdb9820e72c7b5142a309a667
namtran <namtran@namtran-X555LD>
2016-10-05 10:42:46 +0700

    update

--- Algorithm/src/day_7/TravelingSalesman.java
+++ Algorithm/src/day_7/TravelingSalesman.java
@@ -1,13 +1,25 @@
 package day_7;
 
+import java.util.Scanner;
+
 /**
  * @author namtran
- *
  */
 public class TravelingSalesman {
 
+	static int n, cost[][];
+
 	public static void main(String[] args) {
-		System.out.println(123);
+		Scanner scanner = new Scanner(System.in);
+		int m = scanner.nextInt();
+		cost = new int[n][n];
+		for (int index = 0; index < m; index++) {
+			int i = scanner.nextInt();
+			int j = scanner.nextInt();
+			int c = scanner.nextInt();
+			cost[i][j] = c;
+		}
+		scanner.close();
 	}
 
 }
