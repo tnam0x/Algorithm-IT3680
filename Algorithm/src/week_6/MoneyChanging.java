025696cd4997444e8d0ab07d3cc486d4836f57e8
Trần Xuân Nam <namtran4194@gmail.com>
2016-10-12 16:00:23 +0700

    update

--- Algorithm/src/week_6/MoneyChanging.java
+++ Algorithm/src/week_6/MoneyChanging.java
@@ -9,20 +9,23 @@ public class MoneyChanging {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int money = 1000 - scanner.nextInt();
 		int[] currency = { 1, 5, 10, 50, 100, 500 };
-		int count = 0, k = currency.length - 1;
-		while (money > 0) {
-			if (currency[k] <= money) {
-				count += money / currency[k];
-				money = money % currency[k];
+		int testCases = scanner.nextInt();
+		for (int test = 0; test < testCases; test++) {
+			int money = 1000 - scanner.nextInt();
+			int count = 0, k = currency.length - 1;
+			while (money > 0) {
+				if (currency[k] <= money) {
+					count += money / currency[k];
+					money = money % currency[k];
+				}
+				k--;
 			}
-			k--;
-		}
-		if (count == 0) {
-			System.out.println("no solution ");
-		} else {
-			System.out.println(count);
+			if (count == 0) {
+				System.out.println("no solution");
+			} else {
+				System.out.println(count);
+			}
 		}
 		scanner.close();
 	}
