51680ea38148a162a8a9d73e40223484a96e9752
Trần Xuân Nam <namtran4194@gmail.com>
2016-12-22 22:42:53 +0700

    HasCycle

---
+++ Hacker Rank/src/daysofcode/HasCycle.java
@@ -0,0 +1,35 @@
+package daysofcode;
+
+/**
+ * Detect a cycle in a linked list. Note that the head pointer may be 'null'
+ * if the list is empty.
+ * A Node is defined as:
+ */
+public class HasCycle {
+
+	class Node {
+		int data;
+		Node next;
+	}
+
+	/**
+	 * If a loop exists, then the fast pointer will eventually end up behind the
+	 * slow pointer. The fast pointer will then catch up to the slow pointer,
+	 * detecting the loop. This will always happen, no matter the size of the
+	 * loop.
+	 */
+	boolean hasCycle(Node head) {
+		if (head == null)
+			return false;
+		Node slow = head;
+		Node fast = head.next;
+		while (slow != fast) {
+			if (fast == null || fast.next == null)
+				return false;
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return true;
+	}
+
+}
