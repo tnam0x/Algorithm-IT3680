a0390e2292f5b4941aed59201059cd9e5c835178
Trần Xuân Nam <namtran4194@gmail.com>
2017-01-11 23:38:00 +0700

    update

--- Hacker Rank/src/daysofcode/Primality.java
+++ Hacker Rank/src/daysofcode/Primality.java
@@ -6,7 +6,7 @@ public class Primality {
 	 * Worst: O(n) algorithm
 	 */
 	static boolean isPrimeWorst(int n) {
-		if (n < 2) {
+		if (n == 1) {
 			return false;
 		}
 		for (int i = 2; i < n; i++) {
@@ -21,7 +21,7 @@ public class Primality {
 	 * Worst: O(n) algorithm
 	 */
 	static boolean isPrimeLessWorst(int n) {
-		if (n < 2) {
+		if (n == 1) {
 			return false;
 		}
 		for (int i = 2; i < n / 2; i++) {
@@ -36,7 +36,7 @@ public class Primality {
 	 * Best: O( n^(1/2) ) Algorithm
 	 */
 	static boolean isPrimeGood(int n) {
-		if (n < 2) {
+		if (n == 1) {
 			return false;
 		}
 		for (int i = 2; i * i <= n; i++) {
@@ -51,9 +51,9 @@ public class Primality {
 	 * Best: O( n^(1/2) ) Algorithm
 	 */
 	static boolean isPrimeBest(int n) {
-		if (n < 2) {
-			return false;
-		} else if (n == 2) {
+		if (n == 2) {
+			return true;
+		} else if (n == 1 || (n & 1) == 0) {
 			return true;
 		}
 		for (int i = 3; i * i <= n; i += 2) { // i <= Math.sqrt(n)
