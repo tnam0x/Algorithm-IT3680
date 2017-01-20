fb951849751f1cd5e96711fa7523b9cb96685b92
namtran <namtran@namtran-X555LD>
2017-01-20 21:07:54 +0700

    update

--- Hacker Rank/src/daysofcode/BST.java
+++ Hacker Rank/src/daysofcode/BST.java
@@ -1,7 +1,32 @@
 package daysofcode;
 
-// Kiểm tra Binary Tree có là Binary Search Tree
-public class BST {
+public class BST<E extends Comparable<E>> {
+	E[] array;
+
+	// Tim value roi tra ve vi tri trong mang
+	// Neu ko tim duoc thi tra ve -1
+	int binarySearchRecursive(E value, int index, int left, int right) {
+		if (left <= right) {
+			int midIndex = left + ((right - left) / 2);
+			// value is found
+			if (value.equals(array[midIndex]))
+				index = midIndex;
+			else {
+				// search left
+				if (value.compareTo(array[midIndex]) < 0) {
+					right = midIndex - 1;
+				}
+				// search right
+				else {
+					left = midIndex + 1;
+				}
+				index = binarySearchRecursive(value, index, left, right);
+			}
+		}
+		return index;
+	}
+
+	// Kiểm tra Binary Tree có là Binary Search Tree
 	boolean isBST(Node root) {
 		return isBSTUtil(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
 	}
