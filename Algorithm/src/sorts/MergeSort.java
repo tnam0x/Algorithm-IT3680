8d5814418c9889f589eb0c8ff2edad50fa99cb72
Trần Xuân Nam <namtran4194@gmail.com>
2017-02-19 10:17:39 +0700

    update

--- Algorithm/src/sorts/MergeSort.java
+++ Algorithm/src/sorts/MergeSort.java
@@ -11,12 +11,12 @@ public class MergeSort {
 		}
 	}
 
-	public static void sort(int array[], int first, int last) {
-		if (first < last) {
-			int middle = (first + last) / 2;
-			sort(array, first, middle);
-			sort(array, middle + 1, last);
-			merge(array, first, last);
+	public static void sort(int array[], int start, int end) {
+		if (start < end) {
+			int middle = (start + end) / 2;
+			sort(array, start, middle);
+			sort(array, middle + 1, end);
+			merge(array, start, end);
 		}
 	}
 
